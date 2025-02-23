905f459176575fdb676342ea175472893ba6ad5f
psudeoR3BEL <ansh_k@ph.iitr.ac.in>
2025-02-24 01:58:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const addToCartButtons = document.querySelectorAll('.product .add-to-cart');
-
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Product added to cart (Placeholder)');
-        });
-    });
-});
